2a6e86e45861b32e9bc6f87eaeb43ba1077fc61c
Renzo Lazo Siancas <renzo.lazo.siancas@hotmail.com>
2026-08-26 19:09:54 -0500

    rediseño

--- js/script.js
+++
@@ -1,8 +0,0 @@
-  document.querySelectorAll('.cert-tab').forEach(tab=>{
-    tab.addEventListener('click', ()=>{
-      document.querySelectorAll('.cert-tab').forEach(t=>t.classList.remove('active'));
-      document.querySelectorAll('.cert-list').forEach(l=>l.classList.remove('active'));
-      tab.classList.add('active');
-      document.querySelector(`.cert-list[data-list="${tab.dataset.tab}"]`).classList.add('active');
-    });
-  });
